f3eb72e88aabfe23e890d36f3d42f5de88828d74
Baom <bellomouhoutassim@gmail.com>
2018-02-15 13:26:37 +0100

    Correction des stats

--- src/main/java/com/bootcamp/services/LikeService.java
+++ src/main/java/com/bootcamp/services/LikeService.java
@@ -148,7 +148,7 @@ public class LikeService implements DatabaseConstants {
         long dateFin = formatter.parse(endDate).getTime();
         TypedQuery<LikeTable> query = em.createQuery(
                 "SELECT e FROM LikeTable e WHERE e.entityType =?1 AND e.dateCreation BETWEEN ?2 AND ?3", LikeTable.class);
-        List<LikeTable> likes = query.setParameter(1, entityType)
+        List<LikeTable> likes = query.setParameter(1, entityType.name())
                                      .setParameter(2, dateFin)
                                      .setParameter(3, dateDebut)
                                      .getResultList();
